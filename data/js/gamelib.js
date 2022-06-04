59cb1bd834a33f96495eb35b7b1482fe9132acc1
YellowApps <96654406+YellowApps@users.noreply.github.com>
2022-06-04 14:47:59 +0300

    Add files via upload

---
+++ data/js/gamelib.js
@@ -0,0 +1,199 @@
+//GameLib v2 by YellowApps (http://yellowapps.22web.org/)
+
+var Gl2Objects = {};
+
+function Gl2Canvas(name, bg, width, height){
+  this.bgColor = bg; this.width = width; this.height = height;
+
+  this.init = function(){
+    document.body.innerHTML += "<canvas id='" + name + "' width=" + width + " height=" + height + ">ERR_OLD_BROWSER</canvas>";
+    this.ctx = document.getElementById(name).getContext("2d");
+	this.elem = document.getElementById(name);
+
+    var pfs = this.ctx.fillStyle;
+    this.ctx.fillStyle = bg;
+    this.ctx.fillRect(0, 0, this.width, this.height);
+    this.ctx.fillStyle = pfs;
+  }
+}
+
+function Gl2Object(x, y, w, h, d){
+  this.x = x; this.y = y; this.width = w; this.height = h;
+  for(var i in d) this[i] = d[i];
+  this.img = new Image();
+  this.img.src = this.texture;
+  this.img.onload = function(){
+    window["Gl2IsObject" + x + y + w + h + "Loaded"] = true;
+  }
+
+  this.draw = function(cnv){
+    this.canvas = cnv;
+    if(!window["Gl2IsObject" + x + y + w + h + "Loaded"]){
+      setTimeout(function(){Gl2Objects[d.name].draw(Gl2Objects[d.name].canvas);}, 30);
+      return;
+    }
+    cnv.ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  }
+  
+  this.onclick = function(func){
+	  this.clicked = true;
+	  var name = d.name;
+	  
+	  var div = document.createElement("div");
+	  div.id = "d"+name;
+	  div.innerHTML = "&nbsp;";
+	  div.style.position = "absolute";
+	  div.style.cursor = "pointer";
+	  div.style.left = this.x+"px";
+	  div.style.top = this.y+"px";
+	  div.style.width = this.width+"px";
+	  div.style.height = this.height+"px";
+	  div.style.overflow = "hidden";
+	  div.style.fontSize = "9999999px";
+	  div.onclick = func;
+	  
+	  document.body.appendChild(div);
+  }
+  
+  this.delclick = function(){
+	  if(!this.clicked) return;
+	  this.clicked = false;
+	  
+	  document.getElementById("d"+d.name).onclick = null;
+  }
+  
+  this.moveclick = function(x, y){
+	  if(!this.clicked) return;
+	  
+	  var div = document.getElementById("d"+d.name);
+	  div.style.left = x+"px";
+	  div.style.top = y+"px";
+  }
+  
+  Gl2Objects[this.name] = this;
+}
+
+
+function Gl2Entity(x, y, w, h, d){
+  var ent = new Gl2Object(x, y, w, h, d);
+
+  ent.loadAnimations = function(){
+    for(var i in this.animation){
+      for(var j in this.animation[i]){
+        var img = new Image();
+        img.src = this.animation[i][j];
+        this.animation[i][j] = img;
+      }
+    }
+  }
+
+  ent.isCollided = function(r, o){
+    if(r == "left"){
+      return (((this.x >= o.x + o.width) && (this.x <= o.x + o.height)) && ((this.y + this.height >= o.y) && (this.y <= o.y + o.height)));
+    }
+	if(r == "right"){
+      return (((this.x + this.width >= o.x) && (this.x < o.x + o.width)) && ((this.y + this.height > o.y) && (this.y + this.height < o.y + o.height + o.height/2)));
+    }
+	if(r == "top"){
+      return (((this.y <= o.y + o.height) && (this.y > o.y)) && ((this.x + this.width > o.x) && (this.x < o.x + o.width)));
+    }
+	if(r == "bottom"){
+      return (((this.y + this.height >= o.y) && (this.y < o.y)) && ((this.x + this.width > o.x) && (this.x < o.x + o.width)));
+    }
+	if(r == "any"){
+		return (this.isCollided("left", o) || this.isCollided("right", o) || this.isCollided("top", o) || this.isCollided("bottom", o));
+	}
+  }
+  ent.move = function(dir, pixels, delay, steps){
+    var collided = false;
+    for(var i in Gl2Objects){
+      if(Gl2Objects[i].name == this.name) continue;
+      collided = this.isCollided(dir, Gl2Objects[i]);
+      if(collided) break;
+    }
+    if(collided) return;
+
+    //if(this.binded) window.onkeypress = null;
+	
+    function binder(o){}
+
+    if(dir == "left"){
+		this.moveclick(this.x-pixels, this.y);
+      this.canvas.ctx.fillStyle = this.canvas.bgColor;
+      this.canvas.ctx.fillRect(this.x, this.y, this.width, this.height);
+      this.x -= pixels;
+      this.canvas.ctx.drawImage(this.animation.left[0], this.x, this.y, this.width, this.height);
+      var currObj = this;
+      setTimeout(function(){ currObj.canvas.ctx.drawImage(currObj.animation.left[1], currObj.x, currObj.y, currObj.width, currObj.height);binder(currObj); if(steps > 1) currObj.move(dir, pixels, delay, steps-1); }, delay);
+    }else if(dir == "right"){
+		this.moveclick(this.x+pixels, this.y);
+      this.canvas.ctx.fillStyle = this.canvas.bgColor;
+      this.canvas.ctx.fillRect(this.x, this.y, this.width, this.height);
+      this.x += pixels;
+      this.canvas.ctx.drawImage(this.animation.right[0], this.x, this.y, this.width, this.height);
+      var currObj = this;
+      setTimeout(function(){ currObj.canvas.ctx.drawImage(currObj.animation.right[1], currObj.x, currObj.y, currObj.width, currObj.height);binder(currObj); if(steps > 1) currObj.move(dir, pixels, delay, steps-1);}, delay);
+    }else if(dir == "top"){
+		this.moveclick(this.x, this.y-pixels);
+      this.canvas.ctx.fillStyle = this.canvas.bgColor;
+      this.canvas.ctx.fillRect(this.x, this.y, this.width, this.height);
+      this.y -= pixels;
+      this.canvas.ctx.drawImage(this.animation.top[0], this.x, this.y, this.width, this.height);
+      var currObj = this;
+      setTimeout(function(){ currObj.canvas.ctx.drawImage(currObj.animation.top[1], currObj.x, currObj.y, currObj.width, currObj.height);binder(currObj); if(steps > 1) currObj.move(dir, pixels, delay, steps-1);}, delay);
+    }else if(dir == "bottom") {
+		this.moveclick(this.x, this.y+pixels);
+      this.canvas.ctx.fillStyle = this.canvas.bgColor;
+      this.canvas.ctx.fillRect(this.x, this.y, this.width, this.height);
+      this.y += pixels;
+      this.canvas.ctx.drawImage(this.animation.bottom[0], this.x, this.y, this.width, this.height);
+      var currObj = this;
+      setTimeout(function(){ currObj.canvas.ctx.drawImage(currObj.animation.bottom[1], currObj.x, currObj.y, currObj.width, currObj.height);binder(currObj); if(steps > 1) currObj.move(dir, pixels, delay, steps-1); }, delay);
+    }
+  }
+
+  ent.bind = function(keys, pixels, delay){
+    this.binded = true; this.bindKeys = keys; this.bindPixels = pixels; this.bindDelay = delay;
+    var currObj = this;
+    window.onkeypress = function(e){
+      if(e.key == keys.left) currObj.move("left", pixels, delay);
+      if(e.key == keys.right) currObj.move("right", pixels, delay);
+      if(e.key == keys.top) currObj.move("top", pixels, delay);
+      if(e.key == keys.bottom) currObj.move("bottom", pixels, delay);
+    }
+  }
+
+  ent.kill = function(){
+    delete Gl2Objects[this.name];
+
+    this.canvas.ctx.fillStyle = this.canvas.bgColor;
+    this.canvas.ctx.fillRect(this.x, this.y, this.width, this.height);
+
+    if(this.onkill) this.onkill();
+    if(this.binded) window.onkeypress = null;
+  }
+
+  ent.damage = function(n, dl){
+    this.health -= n;
+    this.canvas.ctx.fillStyle = "red";
+    this.canvas.ctx.font = "16px Arial";
+    this.canvas.ctx.fillText(n, this.x + 4, this.y - 2);
+
+    var curr = this, tl = n.toString().length;
+    setTimeout(function(){ curr.canvas.ctx.fillStyle = curr.canvas.bgColor; curr.canvas.ctx.fillRect(curr.x, curr.y - 16, tl * 16, 16); }, dl);
+
+    if(this.health < 0 && !this.undying) this.kill();
+  }
+
+  ent.heal = function(n, dl){
+    this.health += n;
+    this.canvas.ctx.fillStyle = "green";
+    this.canvas.ctx.font = "16px Arial";
+    this.canvas.ctx.fillText(n, this.x + 4, this.y - 2);
+
+    var curr = this, tl = n.toString().length;
+    setTimeout(function(){ curr.canvas.ctx.fillStyle = curr.canvas.bgColor; curr.canvas.ctx.fillRect(curr.x, curr.y - 16, tl * 16, 16); }, dl);
+  }
+
+  return ent;
+}
---
+++ data/js/main.js
@@ -0,0 +1,132 @@
+//undertail code)
+//ver: 0.0.1 (30.05.2022)
+
+//БЛОК ПЕРЕМЕННЫХ
+//переменные wsh
+var shell = WScript.CreateObject("WScript.Shell");
+var fs = WScript.CreateObject("Scripting.FileSystemObject");
+
+//холст
+var canvas = new Gl2Canvas("main-canvas", "black", 658, 478);
+canvas.init();
+
+//БЛОК SAVE-ФАЙЛОВ
+//файл не найден
+if(!fs.FileExists("data/saves/file1.save")){
+	fs.CopyFile("data/saves/file0.save", "data\\saves/file1.save", true);
+}
+//парсинг файла
+var file = JSON.parse(fs.OpenTextFile("data/saves/file1.save", 1).ReadAll());
+
+if(fs.FileExists("data/saves/file9.save")){
+	var tf = fs.OpenTextFile("data/saves/file9.save")
+	window.tmpsave = JSON.parse(tf.ReadAll());
+	tf.Close();
+	file.room = tmpsave.room;
+	
+	setTimeout(function(){ fs.DeleteFile("data/saves/file9.save", true); }, 1000);
+}
+
+var room = JSON.parse(fs.OpenTextFile("data/rooms/" + file.room + ".room", 1).ReadAll());
+
+if(window.tmpsave){
+	file.x = room.spawn[0];
+	file.y = room.spawn[1];
+}
+
+for(var i in room.objects){
+	var obj = room.objects[i];
+	var rand = new Date().getTime();
+	if(obj.type == "object"){
+		var o = new Gl2Object(obj.x, obj.y, obj.width, obj.height, obj.data);
+		o.draw(canvas);
+		o.onclick(eval("(function(){" + obj.onclick + "})"));
+	}else if(obj.type == "entity"){
+		var e = new Gl2Entity(obj.x, obj.y, obj.width, obj.height, obj.data);
+		e.loadAnimations();
+		e.draw(canvas);
+		e.onclick(eval("(function(){" + obj.onclick + "})"));
+	}
+}
+//ОСНОВНОЙ БЛОК
+
+//создание и настройка игрока
+
+var player = new Gl2Entity(file.x, file.y, 75, 75, {name: "player", texture: "data/textures/entity/player/player.png", health: file.health, animation: {left: ["data/textures/entity/player/animation/left1.png","data/textures/entity/player/animation/left2.png"], top: ["data/textures/entity/player/animation/top1.png","data/textures/entity/player/animation/top2.png"], right: ["data/textures/entity/player/animation/right1.png","data/textures/entity/player/animation/right2.png"], bottom: ["data/textures/entity/player/animation/bottom1.png","data/textures/entity/player/animation/bottom2.png"]}});
+player.loadAnimations();
+player.draw(canvas);
+player.bind({left:"a",top:"w",right:"d",bottom:"s"}, 4, 10);
+
+//БЛОК ФУНКЦИЙ
+function message(text, dur){
+	msgd.style.display = "block";
+	msgd.innerHTML = text;
+	setTimeout(function(){
+		msgd.style.display = "none";
+	}, dur);
+}
+
+function save(){
+	file.x = player.x; file.y = player.y;
+	
+	var f = fs.OpenTextFile("data/saves/file1.save", 2);
+	f.Write(JSON.stringify(file));
+	f.Close();
+	
+	message("Файл сохранен", 1000);
+}
+
+function dialog(texts, img, btns, faft, delay){
+	var i = 0;
+	window.text = texts[0][0];
+	dlg.style.display = "block";
+	
+	dlgimg.src = img;
+	
+	function showText(){
+		dlgtxt.innerHTML = "";
+		
+		var a = 0;
+		window.selbtn = 0;
+		setTimeout(function _(){
+			dlgbtns.innerHTML = "";
+			dlgtxt.innerHTML += window.text[a];
+			
+			if(dlgtxt.innerHTML.length < window.text.length){
+				a++;
+				setTimeout(_, delay);
+			}else{
+				if(btns[i]){
+					for(var j in btns[i]){
+						var btn = btns[i][j];
+						window.texts = texts;
+						dlgbtns.innerHTML += "<button style='margin-left: 30px; margin-right: 30px' onclick='window.text = window.texts["+(i+1)+"]["+j+"]'>"+btn+"</button>";
+					}
+				}
+
+				i++;
+
+				if(i >= texts.length){
+					dlg.onclick = function(){ dlg.style.display = "none"; faft(); };
+				}else{
+					if(!btns[i]) window.text = texts[i][0];
+					dlg.onclick = showText;
+				}
+				
+				a = 0;
+				window.selbtn = 0;
+			}
+		}, delay);
+	}
+	showText();
+}
+
+function goToRoom(name){
+	file.room = name;
+	
+	var f = fs.CreateTextFile("data/saves/file9.save", true);
+	f.Write(JSON.stringify(file));
+	f.Close();
+	
+	location.reload();
+}
